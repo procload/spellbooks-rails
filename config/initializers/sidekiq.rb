9d94dbd7d58293c1dcb23f2285c715cb0162464d
Ryan Merrill <me@ryan.cm>
2024-11-06 17:13:24 -0500

    Update Redis configuration for Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,24 +1,33 @@
 require 'sidekiq'
 
 redis_config = if Rails.env.production?
-  { url: ENV['REDIS_URL'],
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+  redis_url = ENV['SIDEKIQ_REDIS_URL'] || ENV['REDIS_TLS_URL'] || ENV['REDIS_URL']
+  
+  {
+    url: redis_url,
+    ssl_params: { 
+      verify_mode: OpenSSL::SSL::VERIFY_NONE 
+    },
     network_timeout: 5,
-    pool_timeout: 5 }
+    pool_timeout: 5,
+    reconnect_attempts: 3,
+    ssl: true
+  }
 else
   { url: 'redis://localhost:6379/1' }
 end
 
+# Log the Redis URL being used (with credentials removed)
+if Rails.env.production?
+  sanitized_url = redis_config[:url].gsub(/\/\/.*@/, '//[FILTERED]@')
+  puts "Connecting to Redis at: #{sanitized_url}"
+end
+
 Sidekiq.configure_server do |config|
   config.redis = redis_config
-  config.logger.level = Logger::DEBUG
+  config.logger.level = Logger::INFO # Change from DEBUG to reduce noise
 end
 
 Sidekiq.configure_client do |config|
   config.redis = redis_config
-end
-
-# Enable Sidekiq logging in development
-if Rails.env.development?
-  Sidekiq.logger.level = Logger::DEBUG
 end 
