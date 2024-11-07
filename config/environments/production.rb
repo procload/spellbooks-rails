6c2259e5895784e7129681e75ddaeb404d8c53b1
Ryan Merrill <me@ryan.cm>
2024-11-07 06:11:26 -0500

    Update database config and remove solid_queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,13 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :redis_cache_store, { 
+    url: ENV['REDIS_URL'],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+    network_timeout: 5,
+    pool_timeout: 5,
+    reconnect_attempts: 3
+  }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :sidekiq
