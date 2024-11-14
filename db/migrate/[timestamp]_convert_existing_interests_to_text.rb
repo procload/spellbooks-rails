bcac10a6f65ff7528a244649279c4e6fa0fdfa16
Ryan Merrill <me@ryan.cm>
2024-11-14 10:27:40 -0500

    Removes old migration

--- db/migrate/[timestamp]_convert_existing_interests_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ConvertExistingInterestsToText < ActiveRecord::Migration[8.0]
-  def up
-    Assignment.find_each do |assignment|
-      if assignment.interests.is_a?(Array)
-        assignment.update_column(:interests, assignment.interests.join(", "))
-      end
-    end
-  end
-end 
