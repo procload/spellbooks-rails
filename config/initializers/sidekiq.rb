0dba8856fbbaeee18860f3c5b56e1c2a74264ff9
Ryan Merrill <me@ryan.cm>
2024-11-19 15:46:52 -0500

    Possible Sidekiq fixes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,21 @@
 require 'sidekiq'
 
-redis_url = ENV.fetch('REDIS_URL') { ENV['SIDEKIQ_REDIS_URL'] }
-# Convert URL to TLS if needed
-redis_url = redis_url.to_s.gsub('redis://', 'rediss://') if ENV['REDIS_TLS_URL'].present?
+redis_config = {
+  url: ENV['REDIS_URL'],  # This will use the TLS URL (port 22640)
+  ssl: true,
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+  reconnect_attempts: 3,
+  timeout: 5,
+  size: 5  # Keep pool size small since we have a 20 connection limit
+}
 
 Sidekiq.configure_server do |config|
-  config.redis = {
-    url: redis_url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
-    reconnect_attempts: 3,
-    size: 10
-  }
+  server_redis_config = redis_config.merge(
+    size: 7  # Larger pool for server
+  )
+  config.redis = server_redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: redis_url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
-    reconnect_attempts: 3,
-    size: 5
-  }
+  config.redis = redis_config
 end
