cab0eab3067ddbe93629b24b00b26b92f4897485
Ryan Merrill <me@ryan.cm>
2024-11-19 14:26:47 -0500

    Update Sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,21 @@
 require 'sidekiq'
 
-redis_conn = proc {
-  url = ENV.fetch('REDIS_URL', ENV['SIDEKIQ_REDIS_URL']).to_s.gsub('rediss://', 'redis://')
-  Redis.new(
-    url: url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
-    reconnect_attempts: 3
-  )
-}
-
-Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 5, &redis_conn)
-end
+redis_url = ENV.fetch('REDIS_URL', ENV['SIDEKIQ_REDIS_URL']).to_s.gsub('rediss://', 'redis://')
 
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 25, &redis_conn)
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+    reconnect_attempts: 3,
+    size: 10
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+    reconnect_attempts: 3,
+    size: 5
+  }
 end
